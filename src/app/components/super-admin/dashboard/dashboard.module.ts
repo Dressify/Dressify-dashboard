cfc65a0ffdd0a4fd236f8dd76e9448742168a193
Hazem Mohamed Omar <55815459+HazemMohamedOmr@users.noreply.github.com>
2023-05-05 23:18:28 +0300

    users Dashboard Temp

--- src/app/components/super-admin/dashboard/dashboard.module.ts
+++ src/app/components/super-admin/dashboard/dashboard.module.ts
@@ -2,13 +2,44 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { SharedModule } from "../../../shared/shared.module";
+import {DashboardComponent} from "./dashboard.component";
+import {HotSellingProductsComponent} from "./hot-selling-products/hot-selling-products.component";
+import {InvoiceOverviewComponent} from "./invoice-overview/invoice-overview.component";
+import {OurActivitiesComponent} from "./our-activities/our-activities.component";
+import {ProductDiscountComponent} from "./product-discount/product-discount.component";
+import {RecentOrdersComponent} from "./recent-orders/recent-orders.component";
+import {RevenueByCategoryComponent} from "./revenue-by-category/revenue-by-category.component";
+import {SalesStatsComponent} from "./sales-stats/sales-stats.component";
+import {SupportComponent} from "./support/support.component";
+import {TotalSaleComponent} from "./total-sale/total-sale.component";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {CarouselModule} from "ngx-owl-carousel-o";
+import {ChartistModule} from "ng-chartist";
+import { NgApexchartsModule } from "ng-apexcharts";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+      DashboardComponent,
+      HotSellingProductsComponent,
+      InvoiceOverviewComponent,
+      OurActivitiesComponent,
+      ProductDiscountComponent,
+      RecentOrdersComponent,
+      RevenueByCategoryComponent,
+      SalesStatsComponent,
+      SupportComponent,
+      TotalSaleComponent
+  ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    SharedModule,
+    CarouselModule,
+    NgbModule,
+    ChartistModule,
+    NgApexchartsModule
   ]
 })
 export class DashboardModule { }
