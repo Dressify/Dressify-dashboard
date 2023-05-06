451c3272fb881fb6e6278665e2c4b22cbf2b23cc
Hazem Mohamed Omar <55815459+HazemMohamedOmr@users.noreply.github.com>
2023-05-06 19:16:03 +0300

    auth routing

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {AdminLoginComponent} from "./admin-login/admin-login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'adminLogin', component: AdminLoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
