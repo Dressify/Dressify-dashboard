93db361c85fb3f1759452e3be61a3ff9ec86fa6e
Hazem Mohamed Omar <55815459+HazemMohamedOmr@users.noreply.github.com>
2023-05-12 08:06:22 +0300

    fixing Navigation error

--- src/app/shared/guard/super-admin.guard.ts
+++ src/app/shared/guard/super-admin.guard.ts
@@ -11,7 +11,7 @@ export class SuperAdminGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.authService.isLoggedIn() && this.authService.getUserRole() === 'SuperAdmin') {
+    if (this.authService.getUserRole() === 'SuperAdmin') {
       return true;
     }
 
--- src/app/shared/guard/vendor.guard.ts
+++ src/app/shared/guard/vendor.guard.ts
@@ -11,7 +11,7 @@ export class VendorGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.authService.isLoggedIn() && this.authService.getUserRole() === 'vendor') {
+    if (this.authService.getUserRole() === 'vendor') {
       return true;
     }
 
